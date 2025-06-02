a812d2283e914fc2aa86c3c4a625c3146ab0a5ee
Nguyễn Đỗ Khải <146650253+NguyenDokhai2005@users.noreply.github.com>
2025-06-02 22:07:30 +0700

    Add files via upload
    UPDATES

---
+++ src/main/java/com/example/DICOM/DTO/UserDTO.java
@@ -0,0 +1,23 @@
+package com.example.DICOM.DTO;
+
+public class UserDTO {
+
+    private Long userId;
+    private String username;
+    private String password;
+    private String role;
+
+    public UserDTO() {}
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+}
