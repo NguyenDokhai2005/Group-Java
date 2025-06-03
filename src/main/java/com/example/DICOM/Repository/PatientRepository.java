6150a7c24c4e1527963cfec9fe9deaf62dac757f
zyrasZ <thaiphat5b@gmail.com>
2025-06-04 03:03:06 +0700

    Update Patient

---
+++ src/main/java/com/example/DICOM/Repository/PatientRepository.java
@@ -0,0 +1,9 @@
+package com.example.DICOM.Repository;
+
+import com.example.DICOM.Entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
