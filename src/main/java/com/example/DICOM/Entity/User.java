a8b2c2088afb909f993a96911936f2ab637897ec
Nguyễn Đỗ Khải <146650253+NguyenDokhai2005@users.noreply.github.com>
2025-06-04 23:29:36 +0700

    update

---
+++ src/main/java/com/example/DICOM/Entity/User.java
@@ -0,0 +1,66 @@
+package com.example.DICOM.Entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+    public User() {}
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
