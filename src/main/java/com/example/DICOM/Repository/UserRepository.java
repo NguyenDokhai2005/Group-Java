6eb6f0a3395e2688c8e5d0184ade5a99f143fd8b
Nguyễn Đỗ Khải <146650253+NguyenDokhai2005@users.noreply.github.com>
2025-06-04 23:26:37 +0700

    Delete src/main/java/com/example/DICOM/Repository/UserRepository.java

--- src/main/java/com/example/DICOM/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.DICOM.Repository;
-
-import com.example.DICOM.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // Optional: findByUsername, etc.
-}
