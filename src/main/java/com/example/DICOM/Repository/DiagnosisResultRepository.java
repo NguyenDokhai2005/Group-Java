58d136bb7b5f94231189bc61a716aa76c830cd94
TruongDuy1212 <duynt5770@ut.edu.vn>
2025-06-06 16:28:22 +0700

    Add files via upload

---
+++ src/main/java/com/example/DICOM/Repository/DiagnosisResultRepository.java
@@ -0,0 +1,13 @@
+package com.example.DICOM.Repository;
+
+import com.example.DICOM.Entity.DiagnosisResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DiagnosisResultRepository extends JpaRepository<DiagnosisResult, Long> {
+    List<DiagnosisResult> findByAnalyzedBy_UserId(Long userId);
+    List<DiagnosisResult> findByImage_Id(Long imageId);
+}
